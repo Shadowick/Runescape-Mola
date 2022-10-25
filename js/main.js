92291be4af0398940f212be8bbd80d5735b86475
Santos <santiagoamola@gmail.com>
2022-10-25 12:19:59 -0300

    Remoción de código de más.

--- js/main.js
+++ js/main.js
@@ -27,15 +27,14 @@ return ingreso;
 }
 
 if (loguear()) {
-    let opciones = prompt("Elegí una habilidad: \n1 - Crafting. \n2 - Smithing. \n3 - Summoning. \nIngresá 'Salir' para finalizar");
-
+    let opciones = "";
     let expHabilidad
 
     while (opciones != "Salir" && opciones != "salir") {
-
+       opciones = prompt("Elegí una habilidad: \n1 - Crafting. \n2 - Smithing. \n3 - Summoning. \nIngresá 'Salir' para finalizar");
     switch(opciones) {
         case "1":
-            /* alert("Elegiste la opción 1"); */
+
             let gemNumber
             let gemExp
             let expSapphire = 50
@@ -43,27 +42,27 @@ if (loguear()) {
             let expRuby = 100
             expHabilidad = parseInt(prompt("Ingrese experiencia actual:"));
             opcionesGemas = prompt("Elegí una gema: \n1 - Sapphire. \n2 - Emerald. \n3 - Ruby.");
-            switch(opcionesGemas) {
-                case "1":
-                    gemNumber = parseInt(prompt("Ingrese el número de gemas Sapp´hire a cortar:"));
-                    gemExp = gemNumber * expSapphire;
-                    alert("Obtendrás " + gemExp + " de experiencia de cortar 'Sapphire'. Alcanzarás la cantidad de " + (expHabilidad + gemExp) + " experiencia.");
-                    break;
-                case "2":
-                    gemNumber = parseInt(prompt("Ingrese el número de gemas Emerald a cortar:"));
-                    gemExp = gemNumber * expEmerald;
-                    alert("Obtendrás " + gemExp + " de experiencia de cortar 'Emerald'. Alcanzarás la cantidad de " + (expHabilidad + gemExp) + " experiencia.");
-                    break;
-                case "3":
-                    gemNumber = parseInt(prompt("Ingrese el número de gemas Ruby a cortar:"));
-                    gemExp = gemNumber * expRuby;
-                    alert("Obtendrás " + gemExp + " de experiencia de cortar 'Ruby'. Alcanzarás la cantidad de " + (expHabilidad + gemExp) + " experiencia.");
-                    break;
-                default:
-                    alert("Ingresó una opción inválida. Por favor, vuelva a intentarlo.");
-                    opcionesGemas = prompt("Elegí una gema: \n1 - Sapphire. \n2 - Emerald. \n3 - Ruby.");
-                    break;
-                }
+                switch(opcionesGemas) {
+                    case "1":
+                        gemNumber = parseInt(prompt("Ingrese el número de gemas Sapphire a cortar:"));
+                        gemExp = gemNumber * expSapphire;
+                        alert("Obtendrás " + gemExp + " de experiencia de cortar 'Sapphire'. Alcanzarás la cantidad de " + (expHabilidad + gemExp) + " experiencia.");
+                        break;
+                    case "2":
+                        gemNumber = parseInt(prompt("Ingrese el número de gemas Emerald a cortar:"));
+                        gemExp = gemNumber * expEmerald;
+                        alert("Obtendrás " + gemExp + " de experiencia de cortar 'Emerald'. Alcanzarás la cantidad de " + (expHabilidad + gemExp) + " experiencia.");
+                        break;
+                    case "3":
+                        gemNumber = parseInt(prompt("Ingrese el número de gemas Ruby a cortar:"));
+                        gemExp = gemNumber * expRuby;
+                        alert("Obtendrás " + gemExp + " de experiencia de cortar 'Ruby'. Alcanzarás la cantidad de " + (expHabilidad + gemExp) + " experiencia.");
+                        break;
+                    default:
+                        alert("Ingresó una opción inválida. Por favor, vuelva a intentarlo.");
+                        break;
+                }           
+
         break;
 
         case "2":
@@ -92,11 +91,9 @@ if (loguear()) {
                 break;
             }else{
                 alert("Ingresó una opción inválida. Por favor, vuelva a intentarlo.");
-                opcionesSmithing = prompt("Elegí un lingote: \n1 - Mithril. \n2 - Adamant. \n3 - Runite.");
                 break;
             }
             
-        break;
 
         case "3":
             let charmNumber
@@ -131,23 +128,16 @@ if (loguear()) {
                 break;
             }else{
                 alert("Ingresó una opción inválida. Por favor, vuelva a intentarlo.");
-                opcionesSummoning = prompt("Elegí un Charm: \nA - Gold Charm. \nB - Green Charm. \nC - Crimsom Charm. \nD - Blue Charm.");
                 break;
             }
-        break;
 
         default:
             break;
   }
 
-opciones = prompt("Elegí una habilidad: \n1 - Crafting. \n2 - Smithing. \n3 - Summoning. \nIngresá 'Salir' para finalizar");
-
 }
 } else{
     alert("Su cuenta fue bloqueada. Por favor, contáctese con soporte.")
 }
-
-
-
 
 alert("Adiós.");
