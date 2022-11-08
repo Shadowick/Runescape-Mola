9ea97f98992414b2d3b72b22073fa072fccc5a6e
SantiMola <santiagoamola@gmail.com>
2022-11-07 22:19:01 -0300

    Modificaciones TP 2

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let usuario2 = "molasico7"
 let contraseñaUsuario2= "987654321"
 
 
-opcionesInicio = prompt("Elige una de las siguiente opciones: \n1 - Calculadora. \n2 - Biblioteca de skills. \nSalir.")
+opcionesInicio = prompt("Elige una de las siguiente opciones: \n1 - Calculadora. \n2 - Items para skills. \nSalir.")
 
 while (opcionesInicio.toLowerCase != "salir") {
     if (opcionesInicio == 1) {
@@ -57,12 +57,12 @@ while (opcionesInicio.toLowerCase != "salir") {
                         case "2":
                             gemNumber = parseInt(prompt("Ingrese el número de gemas Emerald a cortar:"));
                             gemExp = gemNumber * expEmerald;
-                            alert("Obtendrás " + gemExp + " de experiencia de cortar 'Emerald'. Alcanzarás la cantidad de " + (expHabilidad + gemExp) + " experiencia.");
+                            alert(`Obtendrás ${gemExp} de experiencia de cortar 'Emerald'. Alcanzarás la cantidad de ${(expHabilidad + gemExp)} experiencia.`);
                             break;
                         case "3":
                             gemNumber = parseInt(prompt("Ingrese el número de gemas Ruby a cortar:"));
                             gemExp = gemNumber * expRuby;
-                            alert("Obtendrás " + gemExp + " de experiencia de cortar 'Ruby'. Alcanzarás la cantidad de " + (expHabilidad + gemExp) + " experiencia.");
+                            alert(`Obtendrás ${gemExp} de experiencia de cortar 'Ruby'. Alcanzarás la cantidad de ${(expHabilidad + gemExp)} experiencia.`);
                             break;
                         default:
                             alert("Ingresó una opción inválida. Por favor, vuelva a intentarlo.");
@@ -83,17 +83,17 @@ while (opcionesInicio.toLowerCase != "salir") {
                     if(opcionesSmithing==1){
                         barNumber = parseInt(prompt("Ingrese número de lingotes a templar:"));
                         barExp = barNumber * mithrilBar;
-                        alert("Obtendrás " + barExp + " de experiencia de templar lingotes de Mithril. Alcanzarás la cantidad de " + (expHabilidad + barExp) + " experiencia.");
+                        alert(`Obtendrás ${barExp} de experiencia de templar lingotes de Mithril. Alcanzarás la cantidad de ${(expHabilidad + barExp)} experiencia.`);
                         break;
                     }else if(opcionesSmithing==2){
                         barNumber = parseInt(prompt("Ingrese número de lingotes a templar:"));
                         barExp = barNumber * adamantBar;
-                        alert("Obtendrás " + barExp + " de experiencia de templar lingotes de Adamant. Alcanzarás la cantidad de " + (expHabilidad + barExp) + " experiencia.");
+                        alert(`Obtendrás ${barExp} de experiencia de templar lingotes de Adamant. Alcanzarás la cantidad de ${(expHabilidad + barExp)} experiencia.`);
                         break;
                     }else if(opcionesSmithing==3){
                         barNumber = parseInt(prompt("Ingrese número de lingotes a templar:"));
                         barExp = barNumber * runiteBar;
-                        alert("Obtendrás " + barExp + " de experiencia de templar lingotes de Runite. Alcanzarás la cantidad de " + (expHabilidad + barExp) + " experiencia.");
+                        alert(`Obtendrás ${barExp} de experiencia de templar lingotes de Runite. Alcanzarás la cantidad de ${(expHabilidad + barExp)} experiencia.`);
                         break;
                     }else{
                         alert("Ingresó una opción inválida. Por favor, vuelva a intentarlo.");
@@ -115,22 +115,22 @@ while (opcionesInicio.toLowerCase != "salir") {
                     if(opcionesSummoning==="A"||opcionesSummoning==="a"){
                         charmNumber = parseInt(prompt("Ingrese número de charms a utilizar:"));
                         charmExp = charmNumber * goldCharm;
-                        alert("Obtendrás " + charmExp + " de experiencia de utilizar gold charms. Alcanzarás la cantidad de " + (expHabilidad + charmExp) + " experiencia.");
+                        alert(`Obtendrás ${charmExp} de experiencia de utilizar gold charms. Alcanzarás la cantidad de ${(expHabilidad + charmExp)} experiencia.`);
                         break;
                     }else if(opcionesSummoning==="B"||opcionesSummoning==="b"){
                         charmNumber = parseInt(prompt("Ingrese número de charms a utilizar:"));
                         charmExp = charmNumber * greenCharm;
-                        alert("Obtendrás " + charmExp + " de experiencia de utilizar green charms. Alcanzarás la cantidad de " + (expHabilidad + charmExp) + " experiencia.");
+                        alert(`Obtendrás ${charmExp} de experiencia de utilizar green charms. Alcanzarás la cantidad de ${(expHabilidad + charmExp)} experiencia.`);
                         break;
                     }else if(opcionesSummoning==="C"||opcionesSummoning==="c"){
                         charmNumber = parseInt(prompt("Ingrese número de charms a utilizar:"));
                         charmExp = charmNumber * crimsomCharm;
-                        alert("Obtendrás " + charmExp + " de experiencia de utilizar crimsom charms. Alcanzarás la cantidad de " + (expHabilidad + charmExp) + " experiencia.");
+                        alert(`Obtendrás ${charmExp} de experiencia de utilizar crimsom charms. Alcanzarás la cantidad de ${(expHabilidad + charmExp)} experiencia.`);
                         break;
                     }else if(opcionesSummoning==="D"||opcionesSummoning==="d"){
                         charmNumber = parseInt(prompt("Ingrese número de charms a utilizar:"));
                         charmExp = charmNumber * blueCharm;
-                        alert("Obtendrás " + charmExp + " de experiencia de utilizar blue charms. Alcanzarás la cantidad de " + (expHabilidad + charmExp) + " experiencia.");
+                        alert(`Obtendrás ${charmExp} de experiencia de utilizar blue charms. Alcanzarás la cantidad de ${(expHabilidad + charmExp)} experiencia.`);
                         break;
                     }else{
                         alert("Ingresó una opción inválida. Por favor, vuelva a intentarlo.");
@@ -146,7 +146,96 @@ while (opcionesInicio.toLowerCase != "salir") {
             alert("Su cuenta fue bloqueada. Por favor, contáctese con soporte.")
         }
     }else if(opcionesInicio == 2){
+        class Skill {
+
+            constructor(nombreItem, valorItem, expItem, skillTiempoSeg, id){
+                this.nombreItem = nombreItem;
+                this.valorItem = parseInt(valorItem);
+                this.expItem = parseInt(expItem);
+                this.skillTiempoSeg = parseInt(skillTiempoSeg);
+                this.expPorSeg = parseInt(expItem / skillTiempoSeg);
+                this.id = id;
+            }
+        
+            asignarId(array) {
+                this.id = array.length;
+            }
+        }
+        
+        const Skills = [
+        
+            new Skill("Bronze bar" , 10 , 50 , 2 , 1),
+            new Skill("Iron bar" , 40 , 100 , 4 , 2),
+            new Skill("Steel bar" , 100 , 300 , 8 , 3),
+            new Skill("Mithril bar" , 400 , 500 , 14 , 4),
+            new Skill("Adamantium bar" , 1000 , 20 , 300 , 5),
+            new Skill("Rune bar" , 10000 , 2500 , 40 , 6),
+        ]
+        
+        console.log(Skills);
+        
+        let continuar  = true;
+        
+        while(continuar){
+            let ingreso = prompt("Ingresa los datos del Skill: Nombre del Item, Valor del Item, Cantidad de experiencia que brinda, Tiempo que tarda en utilizarlo, separados por una barra diagonal (/). Ingresa X para finalizar");
+        
+            if (ingreso.toUpperCase() == "X") {
+                continuar = false;
+                break;
+            }
+        
+            const datos = ingreso.split("/");
+        
+            const skill = new Skill(datos[0],datos[1],datos[2],datos[3]);
+        
+            Skills.push(skill);
+            skill.asignarId(Skills);
+            console.log(Skills);
+        }
+        
+        let orden = prompt("Elegí el tipo de orden deseado \n1 - Nombre de los Items (A a Z) \n2 - Nombre de los Items (Z a A) \n3 - Menor a mayor duración \n4 - Mayor a menor experiencia. \n5 - Mayor eficiencia de experiencia por segundo. \nIngrese cualquier otro caracter si no quiere ingresar un item nuevo.");
+
+        function ordenar(orden , array) {
+            let arrayOrdenado = array.slice(0);
+            
+        
+            switch (orden) {
+                case "1":
+                    let nombreItemAscendente = arrayOrdenado.sort((a,b)=>a.nombreItem.localeCompare(b.nombreItem));
+                    return nombreItemAscendente;
+        
+                case "2":
+                    let nombreItemDescendente = arrayOrdenado.sort((b,a)=>b.nombreItem.localeCompare(a.nombreItem));
+                    return nombreItemDescendente;
+                
+                case "3":
+                    return arrayOrdenado.sort((a,b)=>b.skillTiempoSeg - a.skillTiempoSeg);
+        
+                case "4":
+                    return arrayOrdenado.sort((a,b)=>a.expItem - b.expItem);
+
+                case "5":
+                    return arrayOrdenado.sort((a,b)=>b.expPorSeg - a.expPorSeg);
+        
+                default:
+                    alert("No es una opción válida. Por favor, intente nuevamente.");
+                    return arrayOrdenado;
+            }
+        }
+        
+        function resultado(array){
+            let lista="";
+        
+            array.forEach(element => {
+                lista+= "Item: "+element.nombreItem +"\nValor: "+element.valorItem +" monedas de oro." + "\nExperiencia:" +element.expItem +" de experiencia." + "\nTiempo de utilización: " +element.skillTiempoSeg +" segundos." + "\nMayor experiencia en el tiempo: " +element.expPorSeg +" exp. por segundo.\n\n"
+            });
+        
+            return lista;
+        }
+        alert(resultado(ordenar(orden,Skills)));
        
+    }else{
+        alert("Ingresó un caracter inválido.");
     }
 }
 alert("Adiós.");
