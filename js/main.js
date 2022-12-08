64ec9c1c310f1907e149836cff20e4bfd7391887
SantiMola <santiagoamola@gmail.com>
2022-12-08 14:04:26 -0300

    Imagenes nuevas

--- js/main.js
+++ js/main.js
@@ -7,58 +7,6 @@ const usuarios = [{
     contraseña: '987654321'
 }]
 
-const items = [{
-    nombre: "Saradomin boots",
-    defensa: "Mágica",
-    precio: 15000,
-    img: './img/Saradomin boots.png'
-}, {
-    nombre: "Saradomin Cape",
-    defensa: "Mágica",
-    precio: 30000,
-    img: './img/Saradomin Cape.png'
-}, {
-    nombre: "Saradomin cuirass",
-    defensa: "Mágica",
-    precio: 400000,
-    img: './img/Saradomin cuirass.png'
-}, {
-    nombre: "Saradomin gaunlets",
-    defensa: "Mágica",
-    precio: 25000,
-    img: './img/Saradomin gaunlets.png'
-}, {
-    nombre: "Saradomin greaves",
-    defensa: "Mágica",
-    precio: 10000,
-    img: './img/Saradomin greaves.webp'
-}, {
-    nombre: "Saradomin helm",
-    defensa: "Mágica",
-    precio: 50000,
-    img: './img/Saradomin helm.png'
-}, {
-    nombre: "Dharok's helm",
-    defensa: "Mágica",
-    precio: 150000,
-    img: "./img/Dharok's helm.png"
-}, {
-    nombre: "Dharok's greataxe",
-    defensa: "Física",
-    precio: 300000,
-    img: "./img/Dharok's greataxe.webp"
-}, {
-    nombre: "Dharok's platebody",
-    defensa: "Física",
-    precio: 500000,
-    img: "./img/Dharok's platebody.webp"
-}, {
-    nombre: "Dharok's platelegs",
-    defensa: "Física",
-    precio: 250000,
-    img: "./img/Dharok's platelegs.webp"
-}]
-
 const inputUsernameLogin = document.getElementById('usernameLogin'),
     inputPassLogin = document.getElementById('passwordLogin'),
     checkRecordar = document.getElementById('recordarme'),
@@ -71,8 +19,8 @@ const inputUsernameLogin = document.getElementById('usernameLogin'),
 
 function validarUsuario(usersDB, user, contraseña) {
     let encontrado = usersDB.find((userDB) => userDB.username == user);
-    console.log(encontrado)
-    console.log(typeof encontrado)
+/*     console.log(encontrado)
+    console.log(typeof encontrado) */
 
     if (typeof encontrado === 'undefined') {
         return false;
@@ -112,7 +60,7 @@ function mostrarInfoItems(array) {
     array.forEach(element => {
         let html = `<div class="card" id="tarjeta${element.nombre}">
                 <h4 class="card-header" id="nombreItem">Nombre: ${element.nombre}</h4>
-                <img src="${element.img}" alt="${element.nombre}" class="card-img-bottom imgItems" id="fotoItem">
+                <img src="${element.img}" alt="${element.nombre}" class="card-img-bottom d-block mx-auto imgItems" id="fotoItem">
                 <div class="card-body">
                     <p class="card-text" id="defensa">Defensa: ${element.defensa}</p>
                     <p class="card-text" id="precio">Precio: ${element.precio} monedas de oro</p>
