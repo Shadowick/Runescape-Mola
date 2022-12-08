0ef90282ac301952f8f3c0f46a620ee9868054ae
SantiMola <santiagoamola@gmail.com>
2022-12-08 18:57:23 -0300

    modificaciones nuevas

--- js/main.js
+++ js/main.js
@@ -7,20 +7,18 @@ const usuarios = [{
     contraseña: '987654321'
 }]
 
+
 const inputUsernameLogin = document.getElementById('usernameLogin'),
     inputPassLogin = document.getElementById('passwordLogin'),
     checkRecordar = document.getElementById('recordarme'),
     btnLogin = document.getElementById('login'),
     modalEl = document.getElementById('modalLogin'),
     modal = new bootstrap.Modal(modalEl),
-    contTarjetas = document.getElementById('tarjetas'),
     elementosToggleables = document.querySelectorAll('.toggeable');
 
 
 function validarUsuario(usersDB, user, contraseña) {
     let encontrado = usersDB.find((userDB) => userDB.username == user);
-/*     console.log(encontrado)
-    console.log(typeof encontrado) */
 
     if (typeof encontrado === 'undefined') {
         return false;
@@ -55,21 +53,6 @@ function saludar(usuario) {
     nombreUsuario.innerHTML = `Bienvenido/a, <span>${usuario.user}</span>`
 }
 
-function mostrarInfoItems(array) {
-    contTarjetas.innerHTML = '';
-    array.forEach(element => {
-        let html = `<div class="card" id="tarjeta${element.nombre}">
-                <h4 class="card-header" id="nombreItem">Nombre: ${element.nombre}</h4>
-                <img src="${element.img}" alt="${element.nombre}" class="card-img-bottom d-block mx-auto imgItems" id="fotoItem">
-                <div class="card-body">
-                    <p class="card-text" id="defensa">Defensa: ${element.defensa}</p>
-                    <p class="card-text" id="precio">Precio: ${element.precio} monedas de oro</p>
-                </div>
-            </div>`;
-        contTarjetas.innerHTML += html;
-    });
-}
-
 function presentarInfo(array, clase) {
     array.forEach(element => {
         element.classList.toggle(clase);
@@ -80,7 +63,6 @@ function estaLogueado(usuario) {
 
     if (usuario) {
         saludar(usuario);
-        mostrarInfoItems(items);
         presentarInfo(elementosToggleables, 'd-none');
     }
 }
@@ -90,12 +72,26 @@ btnLogin.addEventListener('click', (e) => {
     e.preventDefault();
 
     if (!inputUsernameLogin.value || !inputPassLogin.value) {
-        alert('Todos los campos son requeridos');
+        Swal.fire({
+            title: 'Información faltante',
+            text: 'Debe completar ambos campos antes de continuar.',
+            icon: 'info',
+            iconColor: '#0000FF',
+            confirmButtonText: 'Salir',
+            timer: 3000
+        })
     } else {
         let data = validarUsuario(usuarios, inputUsernameLogin.value, inputPassLogin.value);
 
         if (!data) {
-            alert(`Usuario y/o contraseña erróneos`);
+            Swal.fire({
+                title: '¡Oops!',
+                text: 'El usuario y/o la contraseña no son correctos. Por favor, vuelva a intentarlo',
+                icon: 'Error',
+                iconColor: '#66f4ae',
+                confirmButtonText: 'Salir',
+                timer: 3000
+            })
         } else {
 
             if (checkRecordar.checked) {
@@ -106,7 +102,6 @@ btnLogin.addEventListener('click', (e) => {
                 saludar(recuperarUsuario(sessionStorage));
             }
             modal.hide();
-            mostrarInfoItems(items);
             presentarInfo(elementosToggleables, 'd-none');
         }
     }
@@ -118,3 +113,47 @@ btnLogout.addEventListener('click', () => {
 });
 
 estaLogueado(recuperarUsuario(localStorage));
+
+
+
+
+const cardContainer = document.querySelector('#cardContainer');
+const btnMostrar = document.querySelector('#mostrar');
+const seleccionarCategoria = document.querySelector('#categoria');
+
+
+const crearTarjeta = (array) => {
+    cardContainer.innerHTML = ''
+    array.forEach(element => {
+        let card = 
+        `<div class="card">
+            <h4 class="card-header" id="nombreItem">${element.nombre}</h4>
+            <img src="${element.img}" alt="${element.nombre}" class="card-img-bottom d-block mx-auto imgItems" id="fotoItem">
+            <div class="card-body">
+                <p class="card-text" id="tipo">Tipo: ${element.tipo}</p>
+                <p class="card-text" id="precio">Precio: ${element.precio} monedas de oro</p>
+                <a href="${element.url}" class="btn d-flex justify-content-center btn-secondary">Más información</a>
+            </div>
+        </div>`
+        cardContainer.innerHTML += card
+    });
+
+}
+
+function filtrarCategoria(array) {
+    let categoria = seleccionarCategoria.value;
+    if (!categoria) {
+        return array;
+    } else {
+        return array.filter((item) => item.categoria == categoria);
+    }
+}
+
+btnMostrar.addEventListener('click', () => {
+    fetch('./js/items.json')
+        .then((respuesta) => respuesta.json())
+        .then((items) => {
+            localStorage.setItem('item',JSON.stringify(items));
+            crearTarjeta(filtrarCategoria(items));
+        })
+})
